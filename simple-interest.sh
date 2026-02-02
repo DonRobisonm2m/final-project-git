536427c879f782dd53278ed27a4b7f5bc513fe86
Don Robison M2M <robisonm2m@gmail.com>
2026-02-01 22:39:50 -0300

    Delete simple-interest.sh

--- simple-interest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Digite o principal:"
-read p
-echo "Digite a taxa:"
-read r
-echo "Digite o tempo:"
-read t
-s=$(echo "scale=2; $p * $t * $r / 100" | bc)
-echo "Juros simples: $s"
